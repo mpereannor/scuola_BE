d86975fc3d28a104754f3ff6f1a1f6f76d8d1093
chudnofskyATD <mpereannor@gmail.com>
2020-05-04 01:20:28 +0000

    setup: mongodb connected

--- api/server.js
+++ api/server.js
@@ -1,10 +1,37 @@
-const express = require('express');
+const express = require("express");
 const server = express();
+const MongoClient = require("mongodb").MongoClient;
+
+const connectionString =
+  "mongodb+srv://scuolaUser:9060@cluster0-mim7s.mongodb.net/test?retryWrites=true&w=majority";
+
+//callback method
+// MongoClient.connect(
+//   connectedString,
+//   {
+//     useUnifiedTopology: true,
+//   },
+//   (err, client) => {
+//     if (err) return console.log(err);
+//     console.log("Connected to DB");
+//   }
+// );
+
+//promises method 
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client => { 
+        const db = client.db('school-info')
+        server.use()
+        server.get()
+        server.post()
+        server.listen()
+    })
+    .catch(error => console.error(error))
 
 server.use(express.json());
 
-server.get('/', (req, res) => { 
-    res.json('scuola!!!!')
+server.get("/", (req, res) => {
+  res.json("scuola!!!!");
 });
 
 module.exports = server;
