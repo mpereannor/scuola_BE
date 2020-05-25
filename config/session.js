0762cd54eb9fe610211aded9a1ab7e325da43d37
chudnofskyATD <mpereannor@gmail.com>
2020-05-25 01:37:21 +0000

    chore: session updated

--- config/session.js
+++ config/session.js
@@ -1,9 +1,12 @@
-const { IN_PROD } = require('./keys')
+const { IN_PROD } = require('./keys');
+
 const HALF_HOUR = 1000 * 60 * 30;
+const ONE_HOUR = 1000 * 60 * 60;
+const TWO_HOURS = 1000 * 60 * 60 * 2;
 
 const { 
     SESSION_SECRET = 'this is our little secret',
-    SESSION_NAME = 'sid',
+    SESSION_NAME = 'sessionId',
     SESSION_IDLE_TIMEOUT = HALF_HOUR
 } = process.env
 
