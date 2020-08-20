dfe990e1976db04a54416ffbc04bf76ee475dc40
Ubuntu <ubuntu@ip-172-31-25-158.eu-west-2.compute.internal>
2020-08-20 19:15:29 +0000

    chore: dbConnect link updated

--- config/db.js
+++ config/db.js
@@ -13,7 +13,7 @@ mongoose.set("useCreateIndex", true);
 
 const dbConnect = async () => {
   //const connection = await mongoose.connect(process.env.MONGO_URI, {
-  const connection =  await mongoose.connect("mongodb+srv://schooladmin:schooladmin@cluster0-ad7kr.mongodb.net/scuola?retryWrites=true&w=majority", 
+  const connection =  await mongoose.connect("mongodb+srv://schooladmin:schooladmin@cluster0-ad7kr.mongodb.net/scuola?retryWrites=true&w=majority",{ 
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
