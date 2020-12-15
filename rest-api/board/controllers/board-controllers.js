259319698552335e92fc62158fc65679f48ee50b
chudnofskyATD <mpereannor@gmail.com>
2020-12-15 02:43:43 +0000

    refactor: save ref of creator by updating createBoard & getBoardsByCreator controllers

--- rest-api/board/controllers/board-controllers.js
+++ rest-api/board/controllers/board-controllers.js
@@ -1,21 +1,25 @@
 const { Board, Issue } = require("../models/board-models");
 const { User } = require("../../user/models/user-models");
-// const { Unauthorized, BadRequest, notFound, serverError} = require("../../authentication/middlewares/auth-errors");
 const ObjectId = require("mongoose").Types.ObjectId;
-
-const { authorize } = require('../../authentication/middlewares/auth-middleware')
-const { 
-    Unauthorized,
-    BadRequest,
-    notFound,
-    serverError,
-    created,
-    success
+const {
+  authorize,
+} = require("../../authentication/middlewares/auth-middleware");
+const {
+  Unauthorized,
+  BadRequest,
+  notFound,
+  serverError,
+  created,
+  success,
 } = require("../../globals");
 
 async function createBoard(req, res) {
   try {
+    const userId = req.session.userId;
     const board = await Board.create(req.body);
+    const creator = await User.findById(userId);
+    board.creator = creator._id;
+    await board.save();
     // const position  = req.session.position;
     // authorize(req, position);
     res.status(201).json(board);
@@ -52,9 +56,9 @@ async function getBoard(req, res) {
 // async function getBoardByBoardId(req, res) {
 //     const { boardId } = req.query;
 
-//     try{ 
+//     try{
 //         const board = await Board.findById(boardId);
-//         if(!board){ 
+//         if(!board){
 //             notFound();
 //         }
 //         success(board);
@@ -89,128 +93,95 @@ async function archiveBoard(req, res) {
   }
 }
 
-async function linkReportToBoard( req, res) { 
-    try {
-        const { id, report_id } = req.params;
-        const targetBoard = await Board.findByIdAndUpdate(
-            id,
-            { 
-                $push: { 
-                    reports: report_id
-                }
-            }, 
-            {
-                new: true,
-                useFindAndModify: false,
-              }
-        );
-        res.status(200).json(targetBoard);
-
-    } catch (error) {
-        res.status(500).json({
-            message: `something went wrong linking report to board, try again later!
-            ${error.message}`
-          });
-    }
+async function linkReportToBoard(req, res) {
+  try {
+    const { id, report_id } = req.params;
+    const targetBoard = await Board.findByIdAndUpdate(
+      id,
+      {
+        $push: {
+          reports: report_id,
+        },
+      },
+      {
+        new: true,
+        useFindAndModify: false,
+      }
+    );
+    res.status(200).json(targetBoard);
+  } catch (error) {
+    res.status(500).json({
+      message: `something went wrong linking report to board, try again later!
+            ${error.message}`,
+    });
+  }
 }
 
-
-async function getBoardReports(req, res) { 
-    try {
-        const { id } = req.params;
-        const board = await Board.findById(id).populate('reports');
-        const reports = board.reports;
-        res.status(200).json(reports)
-    } catch (error) {
-        res.status(500).json({
-            message: `something went wrong retrieving board reports, try again later!
-            ${error.message}`
-         })
-    }
-
+async function getBoardReports(req, res) {
+  try {
+    const { id } = req.params;
+    const board = await Board.findById(id).populate("reports");
+    const reports = board.reports;
+    res.status(200).json(reports);
+  } catch (error) {
+    res.status(500).json({
+      message: `something went wrong retrieving board reports, try again later!
+            ${error.message}`,
+    });
+  }
 }
-async function addUser( req, res) { 
-    try {
-        const { id, user_id } = req.params;
-        const targetBoard = await Board.findByIdAndUpdate(
-            id,
-            { 
-                $push: { 
-                    reports: user_id
-                }
-            }, 
-            {
-                new: true,
-                useFindAndModify: false,
-              }
-        );
-        res.status(200).json(targetBoard);
-
-    } catch (error) {
-        res.status(500).json({
-            message: `something went wrong adding user to board, try again later!
-            ${error.message}`
-          });
-    }
+async function addUser(req, res) {
+  try {
+    const { id, user_id } = req.params;
+    const targetBoard = await Board.findByIdAndUpdate(
+      id,
+      {
+        $push: {
+          reports: user_id,
+        },
+      },
+      {
+        new: true,
+        useFindAndModify: false,
+      }
+    );
+    res.status(200).json(targetBoard);
+  } catch (error) {
+    res.status(500).json({
+      message: `something went wrong adding user to board, try again later!
+            ${error.message}`,
+    });
+  }
 }
 
-
-async function getUser(req, res) { 
-    try {
-        const { id } = req.params;
-        const board = await Board.findById(id).populate('user');
-        const user = board.user;
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({
-            message: `something went wrong retrieving board user, try again later!
-            ${error.message}`
-         });
-    }
-
+async function getUser(req, res) {
+  try {
+    const { id } = req.params;
+    const board = await Board.findById(id).populate("user");
+    const user = board.user;
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({
+      message: `something went wrong retrieving board user, try again later!
+            ${error.message}`,
+    });
+  }
 }
 
-async function getBoardsByCreator(req, res) { 
-    try{ 
-        const userId = req.session.userId;
-        // const { userId } = req.params;
-        // const userId = req.user._id; 
-        // const { board_id } = req.query;
-        const boards = await User.aggregate([
-            { 
-                $match: { 
-                    _id: userId
-                }
-            },
-            { 
-                $lookup: {
-                    from: "Board",
-                    localField: "_id",
-                    foreignField: "creator",
-                    as: "Board"
-                }
-            }
-            // Board.find(
-        // const boards = await Board.find(
-            // {
-                // 'creator._id' : userId
-            // },
-        //     //  board_id
-        ]    
-        );
-            //  .sort({ 
-                //  createdAt: 1
-            //  })
-            //  .populate('creator')
-        res.status(201).json(boards);
-    } catch(error) { 
-        res.status(500).json({ 
-            message: "something went wrong getting boards created by this user, try again later"
-        })
-    }
+async function getBoardsByCreator(req, res) {
+  try {
+    const userId = req.session.userId;
+    const boards = await Board.find({
+      creator: userId,
+    }).sort({ createdAt: 1 });
+    res.status(200).json(boards);
+  } catch (error) {
+    res.status(500).json({
+      message:
+        "something went wrong getting boards created by this user, try again later",
+    });
+  }
 }
-
-
 
 //groups
 async function createGroupInBoard(req, res) {
@@ -287,7 +258,6 @@ async function archiveGroupInBoard(req, res) {
     });
   }
 }
-
 
 //issues
 async function submitIssue(req, res) {
@@ -369,76 +339,72 @@ async function closeIssueInGroup(req, res) {
   }
 }
 
-async function assignUserToIssue(req, res){ 
-    try {
-        const { id, group_id, issue_id, user_id } = req.params;
-        const targetBoard = await Board.findById(id);
-        const targetGroup = await targetBoard.groups.id(group_id);
-        const issue = await targetGroup.issue.id(issue_id);
-        issue.assigned_user = user_id;
-        await targetBoard.save();
-        res.status(200).json(targetBoard);
-
-    } catch (error) {
-        res.status(500).json({
-            message: "something went wrong assigning issue to user, try again later!",
-          });
-    }
+async function assignUserToIssue(req, res) {
+  try {
+    const { id, group_id, issue_id, user_id } = req.params;
+    const targetBoard = await Board.findById(id);
+    const targetGroup = await targetBoard.groups.id(group_id);
+    const issue = await targetGroup.issue.id(issue_id);
+    issue.assigned_user = user_id;
+    await targetBoard.save();
+    res.status(200).json(targetBoard);
+  } catch (error) {
+    res.status(500).json({
+      message: "something went wrong assigning issue to user, try again later!",
+    });
+  }
 }
 
+async function retrieveAssignedUser(req, res) {
+  try {
+    const { id, group_id, issue_id } = req.params;
+    // const targetBoard = await Board.findById(id);
+    // const targetGroup = await targetBoard.groups.id(group_id);
+    // const issue = await targetGroup.issue.id(issue_id);
+    // issue.populate('assigned_user')
 
-async function retrieveAssignedUser(req, res) { 
-    try {
-        const { id, group_id, issue_id } = req.params;
-        // const targetBoard = await Board.findById(id);
-        // const targetGroup = await targetBoard.groups.id(group_id);
-        // const issue = await targetGroup.issue.id(issue_id);
-        // issue.populate('assigned_user')
-        
-        const targetBoard = await Board.findById(id).populate(
-           `groups(${group_id}).issue(${issue_id}).assigned_user`
-        )
-        res.status(200).json(targetBoard.groups.issue)    
-       
-    } catch (error) {
-        res.status(500).json({
-            message: `something went wrong retrieving assigned user, try again later!
-            ${error.message}`
-          });
-        
-    }
+    const targetBoard = await Board.findById(id).populate(
+      `groups(${group_id}).issue(${issue_id}).assigned_user`
+    );
+    res.status(200).json(targetBoard.groups.issue);
+  } catch (error) {
+    res.status(500).json({
+      message: `something went wrong retrieving assigned user, try again later!
+            ${error.message}`,
+    });
+  }
 }
 
-// async function createBoardTag(req, res){ 
-    
+// async function createBoardTag(req, res){
+
 // }
-// async function getBoardTags(req, res){ 
-    
+// async function getBoardTags(req, res){
+
 // }
-// async function linkReportToIssue(req, res){ 
-    
+// async function linkReportToIssue(req, res){
+
 // }
-// async function getLinkedReports(req, res){ 
-    
+// async function getLinkedReports(req, res){
+
 // }
-// async function unlinkReportToIssue(req, res){ 
-    
+// async function unlinkReportToIssue(req, res){
+
 // }
-// async function addReporterToIssue(req, res){ 
-    
+// async function addReporterToIssue(req, res){
+
 // }
-// async function getIssueReporters(req, res){ 
-    
+// async function getIssueReporters(req, res){
+
 // }
-// async function removeReporterIssue(req, res){ 
-    
+// async function removeReporterIssue(req, res){
+
 // }
 
 module.exports = {
   createBoard,
   getBoards,
   getBoard,
-//   getBoardByBoardId,
+  //   getBoardByBoardId,
   getBoardsByCreator,
   updateBoard,
   archiveBoard,
@@ -453,9 +419,9 @@ module.exports = {
   updateIssueInGroup,
   closeIssueInGroup,
   assignUserToIssue,
-  retrieveAssignedUser, 
+  retrieveAssignedUser,
   linkReportToBoard,
-  getBoardReports, 
-  addUser, 
-  getUser
+  getBoardReports,
+  addUser,
+  getUser,
 };
