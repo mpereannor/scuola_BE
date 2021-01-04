a0a5eda16a1d3619b638f31c276353af49b7655a
chudnofskyATD <mpereannor@gmail.com>
2021-01-04 21:24:25 +0000

    cache: reconfigure redis options

--- config/cache.js
+++ config/cache.js
@@ -1,7 +1,11 @@
 const {
-  REDIS_PORT = 6379,
-  REDIS_HOST = "localhost",
-  REDIS_PASSWORD = "secret",
+    REDIS_HOST= "redis-14791.c44.us-east-1-2.ec2.cloud.redislabs.com",
+    REDIS_PORT = "14791",
+    REDIS_PASSWORD="hw3CRmmg2K7wtQ9nmhLVo2Yg2bBH1i4O",
+
+    // REDIS_HOST = "localhost",
+    // REDIS_PORT = 6379,
+    // REDIS_PASSWORD = "secret",
 } = process.env;
 
 const REDIS_OPTIONS = {
